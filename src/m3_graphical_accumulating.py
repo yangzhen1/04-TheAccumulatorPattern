cfe2fce595ef349052620cda29a980f1d9170d53
yangz4 <yangz4@rose-hulman.edu>
2018-12-03 19:28:46 -0500

    zhen yang

--- src/m3_graphical_accumulating.py
+++ src/m3_graphical_accumulating.py
@@ -9,8 +9,8 @@ Additionally, it emphasizes that you must
 before you can implement a solution to the problem in Python.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher, Mark Hays,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Aaron Wilkin, their colleagues, and Zhen Yang.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -69,6 +69,16 @@ def run_test_draw_parallel_lines():
 
 
 def draw_parallel_lines(n, point, length, window):
+    total = 0
+    x_coordinate = point.x
+    y_coordinate = point.y
+    for _ in range(n):
+        line = rg.Line(rg.Point(x_coordinate, y_coordinate + total),rg.Point(x_coordinate + length, y_coordinate + total))
+        total = total + 30
+        line.attach_to(window)
+
+    window.render()
+
     """
     What comes in: The four arguments are:
       -- A positive integer n.
@@ -96,7 +106,7 @@ def draw_parallel_lines(n, point, length, window):
       :type window: rg.RoseWindow
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     #
     # CONSIDER using the ACCUMULATOR IN GRAPHICS pattern,
@@ -122,7 +132,7 @@ def run_test_draw_lines():
     # TWO tests on ONE window.
     title = 'Tests 1 & 2 of DRAW_LINES:  4 lines, 12 lines!'
     window1 = rg.RoseWindow(350, 400, title)
-
+                    # (20, 120)
     draw_lines(4, rg.Point(20, 120), window1)
     draw_lines(12, rg.Point(150, 230), window1)
     window1.close_on_mouse_click()
@@ -134,6 +144,14 @@ def run_test_draw_lines():
 
 
 def draw_lines(n, point, window):
+
+    for k in range(n):
+        apex = (200 / n) * k + (point.y - 100)
+        line = rg.Line(point, rg.Point(point.x + 100, apex))
+        line.attach_to(window)
+
+    window.render()
+
     """
     What comes in: The three arguments are:
       -- A integer n that is at least 2.
